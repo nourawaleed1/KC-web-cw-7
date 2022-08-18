48a351b85636f94d3a54021c89eb94e923c1ef29
nourawaleed1 <109863728+nourawaleed1@users.noreply.github.com>
2022-08-18 20:33:19 +0300

    made a website to calculate ur weight using js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function BMI(weight=20,height=80){
+    let result=weight/(height*height);
+    // hehe michel jackson 
+    return result
+}
+console.log(BMI(10,40))
+console.log(BMI)
 
+let StatusText = document.getElementById(`result`)
+
+function Status (bmi){
+    if(bmi< 18.5){
+         StatusText.style.backgroundColor='#bde0fe'
+        StatusText.style.color=`orange`
+        return`لديك نقص في الوزن`
+
+    }else if( bmi >=18.5 && bmi <25 ){
+        StatusText.style.backgroundColor='#bde0fe'
+        StatusText.style.color=`green`
+        return`وزنك صحي`
+    }else if(bmi >=25 ){
+        StatusText.style.backgroundColor='#bde0fe'
+        StatusText.style.color=`red`
+        return`لديك زيادة في الوزن`
+    }
+}
+
+
+function calculate(){
+let weight= document.getElementById("weight").value
+let height= document.getElementById("height").value
+
+let bmi= BMI(weight,height)
+let desc= Status(bmi)
+
+let div=document.getElementById("result")
+div.innerText = bmi + " == " + desc
+}
+document.body.style.backgroundColor=`pink`
